3013041879e2c79736af524ba3f416d91d3413f3
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-23 20:52:45 -0500

    Creacion de modelo y mapper respuesta api

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -27,6 +27,10 @@ class ChatProvider extends ChangeNotifier {
 
   Future<void> herReply() async {
     final herMessage = await getYesNoAnswer.getAnswer();
+    messages.add(herMessage);
+
+    notifyListeners();
+    moveScrollToBottom();
   }
 
   Future<void> moveScrollToBottom() async {
